9bd2bfb2e6321231e3de666a0359624a922e60e8
mitsekVasyl <mitsek.vasyl@gmail.com>
2025-02-18 00:04:25 +0200

    POST users: Make first and last name optional fields

--- src/models.py
+++ src/models.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field, EmailStr
 
 
 class UserRequestModel(BaseModel):
     username: str = Field(pattern=r'[A-Za-z0-9]+', max_length=50)
     email: EmailStr = Field(max_length=100)
-    first_name: str = Field(max_length=100)
-    last_name: str = Field(max_length=100)
+    first_name: Optional[str] = Field(max_length=100, default=None)
+    last_name: Optional[str] = Field(max_length=100, default=None)
     age: int = Field(ge=0, le=150)
     password: str = Field(max_length=100)
