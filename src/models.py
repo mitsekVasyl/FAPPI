955510b9580c5d5a716c0a0ea57c94fd9ed4018f
mitsekVasyl <mitsek.vasyl@gmail.com>
2025-02-23 23:59:23 +0200

    Fix migration of is_admin field

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, Integer, VARCHAR, false
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,10 +9,10 @@ class UserModel(Base):
     __tablename__ = 'users'
 
     id: int = Column(Integer, primary_key=True, autoincrement=True)
-    username: str = Column(VARCHAR, unique=True, nullable=False)  # TODO: unique seems have no effect
+    username: str = Column(VARCHAR, unique=True, nullable=False)
     email: str = Column(VARCHAR, unique=True, nullable=False)
     first_name: str = Column(VARCHAR, nullable=True)
     last_name: str = Column(VARCHAR, nullable=True)
     age: int = Column(Integer, nullable=False)
     password: str = Column(VARCHAR, nullable=False)
-    is_admin: bool = Column(Integer, nullable=False, default=False)
+    is_admin: bool = Column(Integer, nullable=False, server_default=false())
