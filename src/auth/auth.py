c410c5b0ccdec02e6f2e8c942d26cf5e5fd8485c
mitsekVasyl <mitsek.vasyl@gmail.com>
2025-03-01 00:53:30 +0200

    Implement JWT token generation

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from src.auth.auth_utils import verify_password
+from src.auth.auth_utils import verify_password, generate_access_token
 from src.database import SessionDep
 from src.models import UserModel
 
@@ -24,4 +24,5 @@ def get_token(form: Annotated[OAuth2PasswordRequestForm, Depends()], dbsesssion:
     if not is_authenticated:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
 
-    return {"access_token": "Bearer MOCKTOKEN", "token_type": "Bearer"}
+    return {"access_token": generate_access_token(user.username, user.id),
+            "token_type": "Bearer"}
--- src/auth/auth_utils.py
+++ src/auth/auth_utils.py
@@ -1,4 +1,13 @@
+from os import getenv
+from datetime import datetime, timezone, timedelta
+
 from bcrypt import gensalt, hashpw, checkpw
+from jose import jwt
+
+SECRET = getenv("JWT_SECRET")
+ALGO = "HS256"
+TOKEN_PREFIX = "Bearer "
+
 
 def hash_password(password: str) -> bytes:
     return hashpw(password.encode('utf-8'), gensalt())
@@ -6,3 +15,9 @@ def hash_password(password: str) -> bytes:
 
 def verify_password(password: str, hashed_password: bytes) -> bool:
     return checkpw(password.encode('utf-8'), hashed_password)
+
+
+def generate_access_token(username: str, user_id: int) -> str:
+    expires = datetime.now(timezone.utc) + timedelta(minutes=30)
+    data = {"exp": expires, "user_id": user_id, "username": username}
+    return TOKEN_PREFIX + jwt.encode(data, key=SECRET, algorithm=ALGO)
