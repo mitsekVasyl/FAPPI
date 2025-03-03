4d93d6bf816a2a25dbe60227aa4702b4f88af315
mitsekVasyl <mitsek.vasyl@gmail.com>
2025-03-03 11:21:10 +0200

    Add access token verification

--- src/auth/auth_utils.py
+++ src/auth/auth_utils.py
@@ -1,12 +1,19 @@
 from os import getenv
 from datetime import datetime, timezone, timedelta
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 from bcrypt import gensalt, hashpw, checkpw
-from jose import jwt
+from jose import jwt, JWTError
+
 
 SECRET = getenv("JWT_SECRET")
 ALGO = "HS256"
 TOKEN_PREFIX = "Bearer "
+
+bearer_dep = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
 
 
 def hash_password(password: str) -> bytes:
@@ -21,3 +28,11 @@ def generate_access_token(username: str, user_id: int) -> str:
     expires = datetime.now(timezone.utc) + timedelta(minutes=30)
     data = {"exp": expires, "user_id": user_id, "username": username}
     return TOKEN_PREFIX + jwt.encode(data, key=SECRET, algorithm=ALGO)
+
+
+def verify_access_token(token: Annotated[str, Depends(bearer_dep)]) -> dict:
+    try:
+        user_info = jwt.decode(token, key=SECRET, algorithms=[ALGO])
+        return user_info
+    except JWTError as ex:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Failed access token verification") from ex
