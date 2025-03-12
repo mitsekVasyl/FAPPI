f1cd81606368fd93357b7d73540adc63f219cebe
mitsekVasyl <70634063+mitsekVasyl@users.noreply.github.com>
2025-03-13 01:46:10 +0200

    Upgrade to livespan events instead of event decorator (#15)

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.database import create_db_and_tables
@@ -5,11 +7,13 @@ from src.users import router as users_router
 from src.auth.auth import router as auth_router
 
 
-app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def on_startup(a: FastAPI):
     create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=on_startup)
 
 
 @app.get('/heartbeat')
