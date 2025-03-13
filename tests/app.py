d599412bfdf3b78c7e42b19130be35958c9ce0b9
mitsekVasyl <mitsek.vasyl@gmail.com>
2025-03-13 11:12:15 +0200

    Use sessionmaker for sqalchemy session

--- tests/app.py
+++ tests/app.py
@@ -1,23 +1,24 @@
 from fastapi.testclient import TestClient
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from src.app import app
 from src.auth.auth_utils import verify_access_token
-from src.database import get_session
+from src.database import get_db_session
 from src.models import Base
 
 
 sqlite_url = "sqlite:///../test_database.db"
 
 engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
+TestSessionLocal = sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
 
 
 def get_test_db_session():
-    with Session(engine) as session:
+    with TestSessionLocal() as session:
         yield session
 
 
@@ -26,9 +27,8 @@ def verify_access_token_override():
 
 
 app.dependency_overrides = {
-    get_session: get_test_db_session,
+    get_db_session: get_test_db_session,
     verify_access_token: verify_access_token_override
 }
 
 test_app = TestClient(app)
-
