71aa6557afbb0b81b900e93b27816ef41d2cf400
mitsekVasyl <70634063+mitsekVasyl@users.noreply.github.com>
2025-03-13 18:32:08 +0200

    Use sessionmaker for sqalchemy session (#16)

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from src.models import Base
 
@@ -12,13 +12,15 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
+SessionLocal = sessionmaker(bind=engine)
+
 def create_db_and_tables():
     Base.metadata.create_all(engine)
 
 
-def get_session():
-    with Session(engine) as session:
+def get_db_session():
+    with SessionLocal() as session:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+SessionDep = Annotated[Session, Depends(get_db_session)]
